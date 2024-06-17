3eb86c51a6233d0ff9026e70c1d52a5cc5d126bd
DataCodeLiteracy <dataliteracy@icloud.com>
2024-06-17 13:04:27 +0900

    [ ⚙️  | | env ] 프로젝트 초기 환경 설정
    2024.06.17

---
+++ src/main/java/com/sim_coding/finder_pets/FinderPetsApplication.java
@@ -0,0 +1,13 @@
+package com.sim_coding.finder_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sim_coding/finder_pets/FinderPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim_coding.finder_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
